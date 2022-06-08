103310c18d22a670f6142627d3008b188ca1b42e
Marina Almeida <67846587+MarinaAlmeida20@users.noreply.github.com>
2022-06-08 12:11:50 +0100

    Footer, Footer.css

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Experience from './pages/Experience';
 import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/projects" element={<Projects />}/>
           <Route path="/experience" element={<Experience />}/>
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
