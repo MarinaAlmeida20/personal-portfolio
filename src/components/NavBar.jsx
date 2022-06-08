9b4d3b8f6273a448f8d478f5cdb0d06fa676f537
Marina Almeida <67846587+MarinaAlmeida20@users.noreply.github.com>
2022-06-08 12:06:44 +0100

    useEffect, useLocation

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,20 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import "../styles/NavBar.css";
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 import ReorderIcon from "@material-ui/icons/Reorder";
+
+import "../styles/NavBar.css";
 
 function NavBar() {
   const [expandNavBar, setExpandNavBar] = useState(false);
 
+  const location = useLocation();
+
+  useEffect(() => {
+    setExpandNavBar(false);
+  }, [location]);
+
   return (
-    <div className="navbar" id={expandNavBar ? "open":"close"}>
+    <div className="navbar" id={expandNavBar ? "open" : "close"}>
       <div className="toggleButton">
         <button onClick={() => setExpandNavBar((prev) => !prev)}>
           <ReorderIcon />
