3fac95139a4db503bddef60c83f076350a41393a
Marina Almeida <67846587+MarinaAlmeida20@users.noreply.github.com>
2022-06-08 11:52:37 +0100

    components and NavBar

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Experience from './pages/Experience';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/projects" element={<Projects />}/>
