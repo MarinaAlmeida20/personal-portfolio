8ebc8a50744c0f1b49b43cf62a4bafa8a6c4113a
Marina Almeida <67846587+MarinaAlmeida20@users.noreply.github.com>
2022-06-10 12:34:30 +0100

    created a new component ProjectDisplay, with params to display the project

---
+++ src/pages/ProjectDisplay.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { ProjectList } from "../helpers/ProjectList";
+
+import GitHubIcon from "@material-ui/icons/GitHub";
+
+
+function ProjectDisplay() {
+  const { id } = useParams();
+  const project = ProjectList[id]; // position inside the projectList file
+  return (
+    <div className="project">
+      <h1>{project.name}</h1>
+      <img src={project.image} alt={project.name}/>
+      <GitHubIcon />
+    </div>
+  );
+}
+
+export default ProjectDisplay;
