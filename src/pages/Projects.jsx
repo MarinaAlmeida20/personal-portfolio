7e4bbef8ed80504dd3cf4dd81335239f0fc0ee3e
Marina Almeida <67846587+MarinaAlmeida20@users.noreply.github.com>
2022-06-10 12:11:35 +0100

    Project main page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import ProjectItem from "../components/ProjectItem";
+// import img
+import Proj1 from "../assets/proj1.jpg"
+import Proj3 from "../assets/proj3.webp"
+
+import "../styles/Project.css"
 
 function Projects() {
   return (
-    <div>Projects</div>
-  )
+    <div className="projects">
+      <h1>My Personal Projects</h1>
+      <div className="projectList">
+        <ProjectItem name="Social Media Website" image={Proj1}/>
+        <ProjectItem name="Spotify Clone" image={Proj3}/>
+      </div>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
