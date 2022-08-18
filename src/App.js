ba46da5ca8b8fa5200078a57c6f78dade0999264
MarinaAlmeida20 <marinabrito.ma@gmail.com>
2022-08-18 21:52:09 +0100

    updating

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 import Home from './pages/Home';
+import NavBar from './components/NavBar';
 import Projects from './pages/Projects';
 import ProjectDisplay from './pages/ProjectDisplay';
 import Experience from './pages/Experience';
-import NavBar from './components/NavBar';
+import Resume from './pages/Resume'
 import Footer from './components/Footer';
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
           <Route path="/projects" element={<Projects />}/>
           <Route path="/project/:id" element={<ProjectDisplay />}/>
           <Route path="/experience" element={<Experience />}/>
+          <Route path="/resume" element={<Resume />}/>
         </Routes>
         <Footer />
       </Router>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
-import EmailIcon from "@material-ui/icons/Email";
+// import EmailIcon from "@material-ui/icons/Email";
+// import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
+// import FilePresentIcon from '@mui/icons-material/FilePresent';
 import GithubIcon from "@material-ui/icons/GitHub";
 import "../styles/Home.css";
 
@@ -11,31 +13,25 @@ function Home() {
         <h2>Hi, My name is Marina</h2>
         <div className="prompt">
           <p>A software developer with a passion for learning and creating.</p>
-          <LinkedInIcon />
-          <EmailIcon />
-          <GithubIcon />
+          <LinkedInIcon className="linkedInIcon" onClick={event =>  window.location.href='https://www.linkedin.com/in/marinaalmeida20/'} />
+          {/* <EmailIcon className="emailIcon" onClick={event =>  window.location.href='https://www.linkedin.com/in/marinaalmeida20/'} /> */}
+          <GithubIcon className="githubIcon" onClick={event =>  window.location.href='https://github.com/MarinaAlmeida20'} />
         </div>
       </div>
       <div className="skills">
         <h1>Skills</h1>
         <ol className="list">
-        <li className="item">
-            <h2> Front-End</h2>
+          <li className="item">
+            <h2> Soft Skills</h2>
             <span>
-              ReactJS, Angular, Redux, HTML, CSS, React Native, Flutter, NPM,
-              Ionic, BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents
+              Problem Solving, Teamwork, Time Management, Adaptability, Communication
             </span>
           </li>
           <li className="item">
-            <h2>Back-End</h2>
+            <h2>Hard Skills</h2>
             <span>
-              NodeJS, Java Spring, .NET, ExpressJS, GraphQL, ApolloServer,
-              MySQL, MongoDB, DynamoDB, DigitalOcean, AWS S3, MS SQL
+            HTML & CSS, ReactJS, NPM, BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents, NodeJS, ExpressJS, PostgresSQL
             </span>
-          </li>
-          <li className="item">
-            <h2>Languages</h2>
-            <span>JavaScript, Java, Python, C#, C, C++, TypeScript, Go</span>
           </li>
         </ol>
       </div>
