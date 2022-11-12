8c9c843dc783a9558a15fa50b02dd5695107239b
MarinaAlmeida20 <marinabrito.ma@gmail.com>
2022-11-12 19:51:30 +0000

    footer identation

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+import Home from './pages/Home';
+import NavBar from './components/NavBar';
+import Projects from './pages/Projects';
+import ProjectDisplay from './pages/ProjectDisplay';
+import Experience from './pages/Experience';
+import Resume from './pages/Resume'
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/projects" element={<Projects />}/>
+          <Route path="/project/:id" element={<ProjectDisplay />}/>
+          <Route path="/experience" element={<Experience />}/>
+          <Route path="/resume" element={<Resume />}/>
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
