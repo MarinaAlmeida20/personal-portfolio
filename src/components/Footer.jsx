2673a83a59dce6eeac42476bc761d570a2057f65
MarinaAlmeida20 <marinabrito.ma@gmail.com>
2022-11-12 19:24:22 +0000

    footer identation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,15 +8,27 @@ function Footer() {
   return (
     <div className="footer">
       <div className="socialMedia">
-        <InstagramIcon className="instagramIcon" onClick={(event) =>
-              (window.location.href = "https://www.instagram.com/marina_mero_almeida/")
-            } />
-        <FacebookIcon className="facebookIcon" onClick={(event) =>
-              (window.location.href = "https://www.facebook.com/marina.almeida.5264382")
-            } />
-        <LinkedInIcon className="linkedInIcon" onClick={(event) =>
-              (window.location.href = "https://www.linkedin.com/in/marinaalmeida20/")
-            } />
+        <InstagramIcon
+          className="instagramIcon"
+          onClick={(event) =>
+            (window.location.href =
+              "https://www.instagram.com/marina_mero_almeida/")
+          }
+        />
+        <FacebookIcon
+          className="facebookIcon"
+          onClick={(event) =>
+            (window.location.href =
+              "https://www.facebook.com/marina.almeida.5264382")
+          }
+        />
+        <LinkedInIcon
+          className="linkedInIcon"
+          onClick={(event) =>
+            (window.location.href =
+              "https://www.linkedin.com/in/marinaalmeida20/")
+          }
+        />
       </div>
       <p>&copy; 2022 Marina Almeida</p>
     </div>
